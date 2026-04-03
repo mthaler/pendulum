7f8a73ea8a91e68c0b04de6b27bce221b3c1767e
Michael Thaler <michael.thaler2@gmail.com>
2026-04-03 10:58:18 +0200

    Updated line.go.

--- line.go
+++ line.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"log"
+
+	"go-hep.org/x/hep/hplot"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotutil"
+	"gonum.org/v1/plot/vg"
+)
+
+func CreateScatterPlot(xs, ys []float64, file string) {
+	p := plot.New()
+	p.Title.Text = "Height - Weight Plot"
+	p.X.Label.Text = "height"
+	p.Y.Label.Text = "weight"
+
+	err := plotutil.AddScatters(p, "", hplot.ZipXY(xs, ys))
+	if err != nil {
+		log.Fatalf("could not create scatter plot: %+v", err)
+	}
+
+	err = p.Save(20*vg.Centimeter, 10*vg.Centimeter, file)
+	if err != nil {
+		log.Fatalf("could not save scatter plot: %+v", err)
+	}
+}
