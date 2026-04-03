931e1aac1ce16316d95977c700a56a97a08c60c2
Michael Thaler <michael.thaler2@gmail.com>
2026-04-03 11:07:17 +0200

    Updated line.go.

--- line.go
+++ line.go
@@ -3,25 +3,23 @@ package main
 import (
 	"log"
 
-	"go-hep.org/x/hep/hplot"
 	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/plotutil"
 	"gonum.org/v1/plot/vg"
 )
 
-func CreateLineplotPlot(xs, ys []float64, file string) {
+func CreateLineplotPlot(points plotter.XYs, file string) {
 	p := plot.New()
-	p.Title.Text = "Height - Weight Plot"
-	p.X.Label.Text = "height"
-	p.Y.Label.Text = "weight"
+	p.Y.Min, p.X.Min, p.Y.Max, p.X.Max = 0, 0, 10, 10
 
-	err := plotutil.AddScatters(p, "", hplot.ZipXY(xs, ys))
+	err := plotutil.AddLines(p, "line1", points)
 	if err != nil {
-		log.Fatalf("could not create scatter plot: %+v", err)
+		log.Fatalf("could not create lineplot: %+v", err)
 	}
 
 	err = p.Save(20*vg.Centimeter, 10*vg.Centimeter, file)
 	if err != nil {
-		log.Fatalf("could not save scatter plot: %+v", err)
+		log.Fatalf("could not save lineplot: %+v", err)
 	}
 }
