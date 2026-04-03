875adb365af0339f3fbe8c6cbef22cf2ff27ae82
Michael Thaler <michael.thaler2@gmail.com>
2026-04-03 09:52:05 +0200

    Updated main.go and pendulum.go.

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package main
+
+import "fmt"
+
+const (
+	x0 = 0.3
+	g  = 9.81
+	l  = 4.0
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+}
--- pendulum.go
+++ pendulum.go
@@ -1,1 +1,8 @@
 package main
+
+import "math"
+
+func x(t float64) float64 {
+	omega := math.Sqrt(g / l)
+	return x0 * math.Cos(omega*t)
+}
