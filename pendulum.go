509d17318b7de2246101879a279eff842bec7d8d
Michael Thaler <michael.thaler2@gmail.com>
2026-04-04 07:20:47 +0200

    Updated pendulum.go.

--- pendulum.go
+++ pendulum.go
@@ -7,7 +7,7 @@ func x(t float64) float64 {
 	return x0 * math.Cos(omega*t)
 }
 
-func phi(t float64) float64 {
+func xdot(t float64) float64 {
 	omega := math.Sqrt(g / l)
-	return phimax * math.Sin(omega*t)
+	return omega
 }
